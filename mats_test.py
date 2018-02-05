d51e159d8713d985cc7dfd2690c3e6e979ec3bdd
Andy Mell <afmell@afmell.com>
2018-02-05 13:12:11 +0000

    Adding tests for case where we request multiple mats

--- mats_test.py
+++ mats_test.py
@@ -11,14 +11,31 @@ class MaterialsTest(unittest.TestCase):
     "Antimony", "Tin", "Germanium" ]), m.names())
 
     def test_closest(self):
+        """
+        Tests a system is chosen where the mat is present
+        """
         m = mats.Materials("mats_test.json")
         self.assertEqual( '164 G. Canis Majoris', m.closest([0, 0, 0], ['Tungsten'])['system'])
         self.assertEqual( '2MASS J10433563-5945136', m.closest([0, 0, 0], ['Germanium'])['system'])
 
     def test_closest_common_mats(self):
+        """
+        Test when a mat is present in both systems that the closest is 
+        chosen
+        """
         m = mats.Materials("mats_test.json")
         self.assertEqual( '164 G. Canis Majoris', m.closest([0, 0, 0], ['Iron'])['system'])
         self.assertEqual( '2MASS J10433563-5945136', m.closest([8000, 0, 3000], ['Iron'])['system'])
+
+    def test_closest_multiple_mats(self):
+        """
+        Check that the closest system with any of the required mats is 
+        chosen
+        """
+        m = mats.Materials("mats_test.json")
+        self.assertEqual( '164 G. Canis Majoris', m.closest([0, 0, 0], ['Tungsten', 'Germanium'])['system'])
+        self.assertEqual( '2MASS J10433563-5945136', m.closest([8000, 0, 3000], ['Tungsten', 'Germanium'])['system'])
+
 
 if __name__ == "__main__":
     unittest.main()
