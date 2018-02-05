fd99284cea975319eac93ddac6fed235def72ee1
Andy Mell <afmell@afmell.com>
2018-02-05 13:07:05 +0000

    Added test that distance is used when mats in multiple systems

--- mats_test.py
+++ mats_test.py
@@ -15,6 +15,11 @@ class MaterialsTest(unittest.TestCase):
         self.assertEqual( '164 G. Canis Majoris', m.closest([0, 0, 0], ['Tungsten'])['system'])
         self.assertEqual( '2MASS J10433563-5945136', m.closest([0, 0, 0], ['Germanium'])['system'])
 
+    def test_closest_common_mats(self):
+        m = mats.Materials("mats_test.json")
+        self.assertEqual( '164 G. Canis Majoris', m.closest([0, 0, 0], ['Iron'])['system'])
+        self.assertEqual( '2MASS J10433563-5945136', m.closest([8000, 0, 3000], ['Iron'])['system'])
+
 if __name__ == "__main__":
     unittest.main()
         
