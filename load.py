a03a9205105a34e1a0ce3233878fb5796d9ece48
Andy Mell <andy@afmell.com>
2018-02-06 18:36:35 +0000

    Making more debug buttons work

--- load.py
+++ load.py
@@ -10,6 +10,7 @@ import sys
 import Queue
 import watcher
 import visited
+import plug
 
 this = sys.modules[__name__]	# For holding module globals
 
@@ -19,18 +20,22 @@ this.debug = 1
 
 
 def dbgFsdCanis():
-    journal_entry("Cmdr", True, "164 G. Canis Majoris", None, {'event': 'FSDJump', 'StarSystem': '164 G. Canis Majoris', 'StarPos': [ 484.125 / -31 / -311.03125]}, {})
+    journal_entry("Cmdr", True, "164 G. Canis Majoris", None, {'event': 'FSDJump', 'StarSystem': '164 G. Canis Majoris', 'StarPos': [ 484.125 , -31 , -311.03125]}, {})
+
 def dbgFsdSol():
     journal_entry("Cmdr", True, "Sol", None, {'event': 'FSDJump', 'StarSystem': 'Sol', 'StarPos': [ 0, 0, 0]}, {})
 
-def dbgScExit():
-    journal_entry("Cmdr", True, "Sol", None, {'event': 'FSDJump'}, {})
+def dbgScExit5ca():
+    journal_entry("Cmdr", True, "164 G. Canis Majoris", "5 c a", {'event': 'SupercruiseExit', 'StarSystem': '164 G. Canis Majoris', 'Body': '5 c a', 'BodyType': 'Planet'}, {})
+    
+def dbgScExit5cd():
+    journal_entry("Cmdr", True, "164 G. Canis Majoris", "5 c d", {'event': 'SupercruiseExit', 'StarSystem': '164 G. Canis Majoris', 'Body': '5 c d', 'BodyType': 'Planet'}, {})
 
 def dbgTouchdownIn():
-    journal_entry("Cmdr", True, "Sol", None, {'event': 'Touchdown'}, {})
+    journal_entry("Cmdr", True, "Sol", None, {'event': 'Touchdown', 'Latitude': -5, 'Longitude': 3}, {})
 
 def dbgTouchdownOut():
-    journal_entry("Cmdr", True, "Sol", None, {'event': 'Touchdown'}, {})
+    journal_entry("Cmdr", True, "Sol", None, {'event': 'Touchdown', 'Latitude': 50, 'Longitude': 30}, {})
 
 def local_file(name):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
@@ -99,20 +104,20 @@ def plugin_app(parent):
 
     # Dynamic Current Location
     nb.Label(this.status_frame, text="Current Lat").grid(row=1, column = 0, sticky=tk.W)
-    this.lat = tk.StringVar()
-    nb.Label(this.status_frame, textvariable=this.lat).grid(row=1, column = 1, sticky=tk.W)
+    this.current_lat = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.current_lat).grid(row=1, column = 1, sticky=tk.W)
 
     nb.Label(this.status_frame, text="Current Lon").grid(row=1, column = 2, sticky=tk.W)
-    this.lon = tk.StringVar()
-    nb.Label(this.status_frame, textvariable=this.lon).grid(row=1, column = 3)
+    this.current_lon = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.current_lon).grid(row=1, column = 3)
 
     nb.Label(this.status_frame, text="Target Lat").grid(row=2, column = 0, sticky=tk.W)
     this.target_lat = tk.StringVar()
-    nb.Label(this.status_frame, textvariable=this.lat).grid(row=2, column = 1, sticky=tk.W)
+    nb.Label(this.status_frame, textvariable=this.target_lat).grid(row=2, column = 1, sticky=tk.W)
 
     nb.Label(this.status_frame, text="Target Lon").grid(row=2, column = 2, sticky=tk.W)
     this.target_lon = tk.StringVar()
-    nb.Label(this.status_frame, textvariable=this.lon).grid(row=2, column = 3)
+    nb.Label(this.status_frame, textvariable=this.target_lon).grid(row=2, column = 3)
 
     # Heading
     nb.Label(this.status_frame, text="Current Heading").grid(row=3, column=0, sticky=tk.W)
@@ -132,9 +137,10 @@ def plugin_app(parent):
     if this.debug:
         nb.Button(this.status_frame, text="FSDJump Sol", command=dbgFsdSol).grid(row=4, sticky=tk.W)
         nb.Button(this.status_frame, text="FSDJump 164 G. Canis Majoris", command=dbgFsdCanis).grid(row=4, column =1, sticky=tk.W)
-        nb.Button(this.status_frame, text="SC Exit", command=dbgScExit).grid(sticky=tk.W)
-        nb.Button(this.status_frame, text="Touchdown In", command=dbgTouchdownIn).grid(row=5, sticky=tk.W)
-        nb.Button(this.status_frame, text="Touchdown Out", command=dbgTouchdownOut).grid(row=5,column =1, sticky=tk.W)
+        nb.Button(this.status_frame, text="SC Exit 5 c d", command=dbgScExit5cd).grid(row=5,column=0,sticky=tk.W)
+        nb.Button(this.status_frame, text="SC Exit 5 c a", command=dbgScExit5ca).grid(row=5,column=1,sticky=tk.W)
+        nb.Button(this.status_frame, text="Touchdown Out", command=dbgTouchdownOut).grid(row=6,column =0, sticky=tk.W)
+        nb.Button(this.status_frame, text="Touchdown In", command=dbgTouchdownIn).grid(row=6, column =1, sticky=tk.W)
       
     this.watcher = watcher.StatusWatcher(local_file("status.json"), this.status_queue)
     this.watcher.daemon = True
@@ -145,8 +151,10 @@ def plugin_app(parent):
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     res = this.events.process(entry, state)
     if res:
+        plug.show_error("Retrieved " + str(len(res)))
         this.action.set(res[0] + ' ' + res[1])
         if len(res) > 3:
             # We have a lat / lon
-            pass
+            this.target_lat.set( str(res[2]) )
+            this.target_lon.set( str(res[3]))
 
