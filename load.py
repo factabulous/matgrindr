e09f3fa74f15dfbd1a03e25fd6d8050dff61262a
Andy Mell <andy@afmell.com>
2018-02-06 17:26:16 +0000

    Updating load.py peparing for compass updates, still a bit broken

--- load.py
+++ load.py
@@ -39,7 +39,7 @@ def plugin_start():
     this.visited = visited.Visited() 
     this.mats = mats.Materials(local_file("mats.json"), this.visited)
     selected = config.get("matgrindr.selected") or []
-    this.events = events.EventEngine(this.mats, selected)
+    this.events = events.EventEngine(this.mats, selected, this.visited)
     return "Matgrindr"
 
 def plugin_stop():
@@ -51,8 +51,10 @@ def update():
     try:
         while True:
             status = this.status_queue.get_nowait()
-            this.lat.set(str(status['Latitude']))
-            this.lon.set(str(status['Longitude']))
+            this.current_lat.set(str(status['Latitude']))
+            this.current_lon.set(str(status['Longitude']))
+            this.current_heading.set(str(status['Heading']))
+            this.current_altitude.set(str(status['Altitude']))
             this.status_frame.update_idletasks()
     except Queue.Empty:
         pass
@@ -90,27 +92,46 @@ def plugin_app(parent):
     app in the main window
     """
     this.status_frame = nb.Frame(parent)
-  
+ 
+    # Current Action being recommended 
     this.action = tk.StringVar() 
     nb.Label(this.status_frame, textvariable=this.action).grid(row=0, column = 0, sticky=tk.W)
-    nb.Label(this.status_frame, text="Lat").grid(row=1, column = 0, sticky=tk.W)
+
+    # Dynamic Current Location
+    nb.Label(this.status_frame, text="Current Lat").grid(row=1, column = 0, sticky=tk.W)
     this.lat = tk.StringVar()
     nb.Label(this.status_frame, textvariable=this.lat).grid(row=1, column = 1, sticky=tk.W)
 
-    nb.Label(this.status_frame, text="Lon").grid(row=1, column = 2, sticky=tk.W)
+    nb.Label(this.status_frame, text="Current Lon").grid(row=1, column = 2, sticky=tk.W)
     this.lon = tk.StringVar()
     nb.Label(this.status_frame, textvariable=this.lon).grid(row=1, column = 3)
 
-    nb.Label(this.status_frame, text="Heading").grid(row=2, column=0, sticky=tk.W)
-    this.heading = tk.StringVar()
-    nb.Label(this.status_frame, textvariable=this.heading).grid(row=2, column=1)
-    nb.Label(this.status_frame, text="Attitude").grid(row=1, column=2, sticky=tk.W)
-    this.attitude = tk.StringVar()
-    nb.Label(this.status_frame, textvariable=this.attitude).grid(row=2, column=3)
+    nb.Label(this.status_frame, text="Target Lat").grid(row=2, column = 0, sticky=tk.W)
+    this.target_lat = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.lat).grid(row=2, column = 1, sticky=tk.W)
 
+    nb.Label(this.status_frame, text="Target Lon").grid(row=2, column = 2, sticky=tk.W)
+    this.target_lon = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.lon).grid(row=2, column = 3)
+
+    # Heading
+    nb.Label(this.status_frame, text="Current Heading").grid(row=3, column=0, sticky=tk.W)
+    this.current_heading = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.current_heading).grid(row=3, column=1)
+    nb.Label(this.status_frame, text="Altitude").grid(row=3, column=2, sticky=tk.W)
+    this.current_altitude = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.current_altitude).grid(row=3, column=3)
+
+    # Target Heading
+    nb.Label(this.status_frame, text="Target Heading").grid(row=3, column=0, sticky=tk.W)
+    this.target_heading = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.target_heading).grid(row=3, column=1)
+    nb.Label(this.status_frame, text="Attitude").grid(row=3, column=2, sticky=tk.W)
+    this.target_attitude = tk.StringVar()
+    nb.Label(this.status_frame, textvariable=this.target_attitude).grid(row=3, column=3)
     if this.debug:
-        nb.Button(this.status_frame, text="FSDJump Sol", command=dbgFsdSol).grid(row=3, sticky=tk.W)
-        nb.Button(this.status_frame, text="FSDJump 164 G. Canis Majoris", command=dbgFsdCanis).grid(row=3, column =1, sticky=tk.W)
+        nb.Button(this.status_frame, text="FSDJump Sol", command=dbgFsdSol).grid(row=4, sticky=tk.W)
+        nb.Button(this.status_frame, text="FSDJump 164 G. Canis Majoris", command=dbgFsdCanis).grid(row=4, column =1, sticky=tk.W)
         nb.Button(this.status_frame, text="SC Exit", command=dbgScExit).grid(sticky=tk.W)
         nb.Button(this.status_frame, text="Touchdown In", command=dbgTouchdownIn).grid(row=5, sticky=tk.W)
         nb.Button(this.status_frame, text="Touchdown Out", command=dbgTouchdownOut).grid(row=5,column =1, sticky=tk.W)
@@ -122,5 +143,10 @@ def plugin_app(parent):
     return this.status_frame
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
-    this.events.process(entry, state)
+    res = this.events.process(entry, state)
+    if res:
+        this.action.set(res[0] + ' ' + res[1])
+        if len(res) > 3:
+            # We have a lat / lon
+            pass
 
