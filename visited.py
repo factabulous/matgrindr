a0e4828e05a8ff798d4ae52f55fc6c64bc2ed12b
Andy Mell <afmell@afmell.com>
2018-02-07 11:23:43 +0000

    Finishing off implementation of the visited code

--- visited.py
+++ visited.py
@@ -15,29 +15,60 @@ class Visited():
            system / body / latitude / longitude / visit_time
        """
        self._visited = json.loads(state)
-       self._interval_days = 7
+       self._interval_secs = interval_days * 24 * 3600
+
+    def expired(self, t, now):
+        """
+        Helper to say of time 't' is expired with respect to the 
+        now time using the interval specified in the constructor
+        """
+
+        how_long = now - t
+        return how_long >= self._interval_secs
 
     def save(self, when=time.time()):
         """
         Returns a string representation of the visited status such that it
         can be stored in a string
+
+        when allows you to specify the time now - mainly for testing
         """
-        return json.dumps(self._visited)
+        return json.dumps([ x for x in self._visited if not self.expired(x['at'], when)])
+
+    def find(self, location):
+        """
+        Finds a location in the store, or return None if not present
+        """
+        for v in self._visited:
+            if v['system'] == location['system'] and v['body'] == location['body'] and v['lat'] == location['lat'] and v['lon'] == location['lon']:
+                return v
+        return None
 
     def set_visited(self, location, when=time.time()):
         """
         Sets a location as visited. Location is expected to be a dict 
-        containing system / body / latitude / longitude
+        containing system / body / lat / lon
+        when allows you to specify the time now - mainly for testing
         """
-        pass # For now - needs implementing
+        v = self.find(location)
+        if v:
+            v['at'] = when
+        else:
+            location['at'] = when
+            self._visited.append(location)
 
     def is_visited(self, location, when=time.time()):
         """
         Asks if a location is visited. Location is expected to be a dict 
-        containing system / body / latitude / longitude
+        containing system / body / lat / lon
+        when allows you to specify the time now - mainly for testing
         Returns True or False
         """
-        return False # For now - needs implementing
+        v = self.find(location)
+        if v and not self.expired(v['at'], when):
+            return True
+        return False
+        
 
 
 
--- visited_test.py
+++ visited_test.py
@@ -4,11 +4,33 @@ import unittest
 import visited
 
 class VisitedTest(unittest.TestCase):
+
     def test_visited_recently(self):
         v = visited.Visited()
         v.set_visited( { 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, 
                  when = 1000 )
         self.assertTrue(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=1000))
+        self.assertFalse(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=1000 + 7 * 24 * 3600))
+
+    def test_visited_multiple_times(self):
+        v = visited.Visited()
+        v.set_visited( { 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, 
+                 when = 1000 )
+        self.assertTrue(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=1000))
+        self.assertFalse(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=1000 + 7 * 24 * 3600))
+        v.set_visited( { 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, 
+                 when = 3000 )
+        # Now should be using the new recorded visit at 3000 secs
+        self.assertTrue(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=1000 + 7 * 24 * 3600))
+        self.assertFalse(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=3000 + 7 * 24 * 3600))
+
+    def test_save(self):
+        v = visited.Visited()
+        v.set_visited( { 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, 
+                 when = 1000 )
+        self.assertTrue(v.is_visited({ 'system': 'Sol', 'body': 'Earth', 'lat': 0, 'lon': 0 }, when=1000))
+        self.assertNotEqual('[]', v.save(when=1000))
+        self.assertEqual('[]', v.save(when=1000 + 7 * 24 * 3600))
 
 
 if __name__ == '__main__':
