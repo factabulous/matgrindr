172ad47305d380f53c69579c78418b95b267eb9c
Andy Mell <afmell@afmell.com>
2018-02-09 10:52:52 +0000

    Adding ability to work out the vertical angle for descent

--- heading.py
+++ heading.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 
-import math
+from math import log, radians, degrees, sin, pi, tan, cos, asin, atan2, fabs, sqrt
 
 def heading(start, end):
     """
     Find how to get from the point on a planet specified as a tuple start
     to a point specified in the tuple end
     """
-    start = ( math.radians(start[0]), math.radians(start[1]))
-    end = ( math.radians(end[0]), math.radians(end[1]))
+    start = ( radians(start[0]), radians(start[1]))
+    end = ( radians(end[0]), radians(end[1]))
 
     delta_lon = end[1] - start[1]
-    delta_lat = math.log(math.tan(math.pi/4 + end[0]/2)/math.tan(math.pi/4 + start[0]/2))
-    return int(round((360 + math.degrees(math.atan2(delta_lon, delta_lat))) % 360))
+    delta_lat = log(tan(pi/4 + end[0]/2)/tan(pi/4 + start[0]/2))
+    return int(round((360 + degrees(atan2(delta_lon, delta_lat))) % 360))
+
+def great_circle(start, end, radius):
+    start = ( radians(start[0]), radians(start[1]))
+    end = ( radians(end[0]), radians(end[1]))
+    delta_lat = fabs(start[0] - end[0])
+    delta_lon = fabs(start[1] - end[1])
+
+    delta = 2 * asin( sqrt( 
+        pow(sin(delta_lat / 2), 2) + cos(start[0]) * cos(end[0]) * pow(sin(delta_lon/2), 2)))
+    return radius * delta
+
+def rate_of_descent(start, end, height, radius):
+    """
+    Works out the rate of descent needed to hit zero height at the 
+    given end point
+    """
+    horz_distance = great_circle(start, end, radius)
+    angle = atan2(-height, horz_distance)
+    return int(round(degrees(angle)))
--- heading_test.py
+++ heading_test.py
@@ -5,10 +5,27 @@ import unittest
 
 class HeadingTest(unittest.TestCase):
 
-    def test_simple(self):
+    def test_heading(self):
         self.assertEqual(90, heading.heading((0, 0), (0, 45)))
         self.assertEqual(264, heading.heading((10, 20), (-10, -170)))
         self.assertEqual(185, heading.heading((90, 179), (0, 0)))
+
+    def test_great_circle(self):
+        self.assertAlmostEqual(87.266, heading.great_circle((0, 0), (0, 10), 500), places=3)
+        self.assertAlmostEqual(87.266, heading.great_circle((0, 0), (0, -10), 500), places=3)
+        self.assertAlmostEqual(87.266, heading.great_circle((0, 0), (-10, 0), 500), places=3)
+        self.assertAlmostEqual(87.266, heading.great_circle((0, 0), (10, 0), 500), places=3)
+        self.assertAlmostEqual(174.5329, heading.great_circle((0, 0), (0, 20), 500), places=3)
+
+    def test_rate_of_descent(self):
+        """
+        Works out the rate of descent needed to hit zero height at the 
+        given end point
+        """
+        self.assertEqual(-45, heading.rate_of_descent((0,0), (0,10), radius=500, height=87))
+        self.assertEqual(-63, heading.rate_of_descent((0,0), (0,10), radius=500, height=174))
+        
+
 
 if __name__ == "__main__":
     unittest.main()
