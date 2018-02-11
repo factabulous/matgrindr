fd3452613550ba7c94883af35274ded26a4c7fa7
Andy Mell <andy@afmell.com>
2018-02-11 12:57:18 +0000

    Adding a grid helper to the util module

--- util.py
+++ util.py
@@ -8,3 +8,33 @@ def same(left, right):
     """
     return left.upper() == right.upper()
 
+class GridHelper():
+    """
+    Helper for Tk grid formatting - vends row and col numbers
+    so that I don't have to keep altering them ;)
+    """
+
+    def __init__(self):
+        self._row = 0
+        self._col = 0
+
+    def col(self, columnspan = 1):
+        """
+        Returns the next column
+        """
+        v = self._col
+        self._col = self._col + columnspan
+        return v
+
+    def row(self):
+        """
+        Returns the current row (does not increment - use newrow() for that)
+        """
+        return self._row
+
+    def newrow(self):
+        self._row = self._row + 1
+        self._col = 0
+        return self._row
+
+
